155fe6c1c24c72792527081211b482d3490a204c
geoyee <geoyee@yeah.net>
2021-08-16 14:15:34 +0800

    Init BMap

--- camera.py
+++ camera.py
@@ -4,29 +4,36 @@ import os.path as ops
 
 
 class Camera(object):
-  def __init__(self, c_mode=0, c_width=768, c_height=768)
-    if c_mode not in [0, 1]:
-      raise ValueError("The c_mode must be 0 or 1!")
-    self.cap = cv2.VideoCapture(c_mode)  # 0：电脑摄像头，1：外接摄像头
-    self.cap.set(3, c_width)  # 宽
-    self.cap.set(4, c_height)  # 高
+    def __init__(self, 
+                 c_mode=0, 
+                 c_width=768, 
+                 c_height=768,
+                 updata_func=None):
+        # 0：电脑摄像头，1：外接摄像头
+        if c_mode not in [0, 1]:
+            raise ValueError("The c_mode must be 0 or 1!")
+        self.cap = cv2.VideoCapture(c_mode)
+        self.cap.set(3, c_width)  # 宽
+        self.cap.set(4, c_height)  # 高
+        self.updata_func = updata_func  # 视频显示方法
 
-  def get_img(self, save_path=None):
-    while(self.cap.isOpened()):
-      ret_flag, Vshow = self.cap.read()  # 是否正确，图像
-      if not ret_flag:
-        print("Failed to get image")
-      cv2.imshow('Capture', Vshow)
-      # q退出
-      usdown = cv2.waitKey(0)
-      if usdown == "q":
-        self.cap.release()
-        cv2.destoryAllWindows()
-      time.sleep(0.1)  # 每0.1秒获取一次
-      # 保存
-      if save_path is not None:
-        img_path = ops.join(
-          save_path,
-          (time.time().replace(".", "_") + ".jpg")
-        )
-        cv2.imwrite(img_path, Vshow)
+    def get_img(self, save_path=None):
+        while(self.cap.isOpened()):
+            ret_flag, Vshow = self.cap.read()  # 是否正确，图像
+            if not ret_flag:
+                raise ValueError("Failed to get image")
+            if self.updata_func is not None:
+                Vshow = self.updata_func(Vshow)
+            cv2.imshow('Capture', Vshow)
+            # q退出
+            usdown = cv2.waitKey(0)
+            if usdown == "q":
+                self.cap.release()
+                cv2.destoryAllWindows()
+            time.sleep(0.1)  # 每0.1秒获取一次
+            # 保存
+            if save_path is not None:
+                img_path = ops.join(
+                    save_path,
+                    (time.time().replace(".", "_") + ".jpg"))
+                cv2.imwrite(img_path, Vshow)
