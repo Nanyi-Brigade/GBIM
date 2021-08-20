c7cf68201f293dc4ed9214ed5223791ea3b61c16
leaf59 <641865512@qq.com>
2021-08-20 16:19:37 +0800

    test v1

--- handclas/infer.py
+++ handclas/infer.py
@@ -1,10 +1,15 @@
 import paddlex as pdx
+from paddlex import transforms as T
 
+
+transforms = T.Compose([
+    T.ResizeByShort(short_size=256), T.CenterCrop(crop_size=224), T.Normalize()
+])
 
 class HandClas():
     def __init__(self, params_path):
         self.model = pdx.load_model(params_path)
 
     def get_clas(self, img):
-        result = self.model.predict(img, topk=5)
+        result = self.model.predict(img, topk=5, transforms=transforms)
         return result
